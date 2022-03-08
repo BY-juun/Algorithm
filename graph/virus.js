6978281a8286b993d0fce22c319001734870dcfc
ByeongJun <neostgeart@ajou.ac.kr>
2022-03-09 01:22:05 +0900

    22/03/09

--- graph/virus.js
+++ graph/virus.js
@@ -2,7 +2,6 @@ let graph = [];
 let isVisited = [];
 let row, col;
 let tempArr;
-let direction;
 let dx = [0, 0, 1, -1];
 let dy = [1, -1, 0, 0];
 
@@ -10,7 +9,6 @@ const solve = () => {
   let totalLength = row * col;
   let answer = 0;
   for (let i = 0; i < totalLength; i++) {
-    //console.log(graph);
     if (graph[i] === 0) {
       graph[i] = 1; //벽 세우기
       for (let j = i + 1; j < totalLength; j++) {
@@ -20,18 +18,20 @@ const solve = () => {
             if (graph[k] === 0) {
               graph[k] = 1; //벽 세우기 (3개 다 세움)
               tempArr = [...graph];
-
               spreadVirus();
               const result = findSafeArea();
-              if (result > answer) answer = result;
+              if (result > answer) {
+                answer = result;
+                console.log(tempArr);
+              }
+              graph[k] = 0;
             }
-            graph[k] = 0;
           }
+          graph[j] = 0;
         }
-        graph[j] = 0;
       }
+      graph[i] = 0;
     }
-    graph[i] = 0;
   }
   console.log(answer);
 };
@@ -83,7 +83,6 @@ rl.on("line", function (line) {
       isVisited.push(Number(value));
     });
   }
-  direction = [col, -col, +1, -1];
   solve();
   process.exit();
 });
