b811eb8e3848eb4e475f7c5c553048b8967d8554
ByeongJun <neostgeart@ajou.ac.kr>
2022-11-25 15:06:32 +0900

    22/11/25

--- Programmers/2022kaKaoInternShipLv2.js
+++ Programmers/2022kaKaoInternShipLv2.js
@@ -1,24 +1,37 @@
 const sum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
+const isInteger = (num) => (Math.floor(num) !== num ? false : true);
+const isOrigin = (leftIdx, leftOriginIdx, halfIdx, halfOriginIdx) => leftIdx === leftOriginIdx && halfIdx === halfOriginIdx;
 
 function solution(queue1, queue2) {
-  var answer = -1;
-  const TOTAL_ARRAY = [...queue1, ...queue2];
-  const MAX_COUNT = TOTAL_ARRAY.length * 4;
-  const TARGET = sum(TOTAL_ARRAY) / 2;
-  let start = 0;
-  let end = queue1.length;
-  let count = 0;
-  let queue1Sum = sum(TOTAL_ARRAY.slice(start, end));
-  while (count <= MAX_COUNT && start <= end) {
-    if (queue1Sum === TARGET) return count;
-    else if (queue1Sum < TARGET) {
-      queue1Sum += TOTAL_ARRAY[end];
-      end++;
-    } else if (queue1Sum > TARGET) {
-      queue1Sum -= TOTAL_ARRAY[start];
-      start++;
+  var answer = 0;
+  const queue = [...queue1, ...queue2];
+  const halfSum = sum(queue) / 2;
+
+  if (!isInteger(halfSum)) return -1;
+
+  const length = queue.length;
+
+  const leftOriginIdx = 0;
+  const halfOriginIdx = queue1.length - 1;
+
+  let leftIdx = 0;
+  let halfIdx = queue1.length - 1;
+
+  let leftSum = sum(queue1);
+
+  while (true) {
+    if (leftSum === halfSum) break;
+    else if (leftSum < halfSum) {
+      halfIdx = (halfIdx + 1) % length;
+      leftSum += queue[halfIdx];
+    } else {
+      leftSum -= queue[leftIdx];
+      leftIdx = (leftIdx + 1) % length;
     }
-    count++;
+
+    if (isOrigin(leftIdx, leftOriginIdx, halfIdx, halfOriginIdx)) return -1;
+    if (leftIdx > halfIdx) return -1;
+    answer++;
   }
 
   return answer;
