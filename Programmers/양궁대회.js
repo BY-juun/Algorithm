1b0402cbf7353d44755bc2fe99a7e8dca680efd9
ByeongJun <neostgeart@ajou.ac.kr>
2022-11-25 16:54:58 +0900

    22/11/25

--- Programmers/양궁대회.js
+++ Programmers/양궁대회.js
@@ -1,49 +1,64 @@
-let prevScore = Number.MIN_SAFE_INTEGER;
+let maxScoreGap = Number.MIN_SAFE_INTEGER;
 let answer = [];
-let Apeach;
-let ArrowLimit;
+const MAX_SCORE = 10;
+const SAME_SCORE = 0;
 
-const sum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
-
-function getScore(Ryan, Apeach) {
-  let RyanScore = 0;
-  let ApeachScore = 0;
-  for (let i = 0; i <= 10; i++) {
-    const score = 10 - i;
-    if (Ryan[i] === 0 && Apeach[i] === 0) continue;
-    if (Ryan[i] > Apeach[i]) RyanScore += score;
-    else ApeachScore += score;
-  }
-  return ApeachScore > RyanScore ? 0 : RyanScore - ApeachScore;
+function isLastIdx(idx, length) {
+  return idx === length - 1 ? true : false;
 }
 
-function DFS(curIdx, arr) {
-  if (curIdx === 11) {
-    const score = getScore(arr, Apeach);
-    if (score !== 0) {
-      if (score > prevScore) {
-        prevScore = score;
-        answer = [...arr];
-      } else if (score === prevScore) {
-        for (let i = 10; i >= 0; i--) {
-          if (answer[i] < arr[i]) return (answer = [...arr]);
-          else if (answer[i] > arr[i]) return;
-        }
-      }
+function isScoreExist(lion, appeach) {
+  return lion === 0 && appeach === 0 ? false : true;
+}
+
+function isEnd(idx, length) {
+  return idx === length ? true : false;
+}
+
+function compareSameScore(scores) {
+  for (let i = scores.length - 1; i >= 0; i--) {
+    if (scores[i] > answer[i]) return true;
+    else if (scores[i] < answer[i]) return false;
+  }
+  return false;
+}
+
+function compareScore(ryan, appeach) {
+  let lionScore = 0;
+  let appeachScore = 0;
+  for (let i = 0; i < ryan.length; i++) {
+    const score = MAX_SCORE - i;
+    if (!isScoreExist(ryan[i], appeach[i])) continue;
+    if (ryan[i] > appeach[i]) lionScore += score;
+    else appeachScore += score;
+  }
+  return lionScore > appeachScore ? { isRyanWin: true, scoreGap: lionScore - appeachScore } : { isRyanWin: false, scoreGap: 0 };
+}
+
+function DFS(idx, remainedArrows, ryan, appeach) {
+  if (isEnd(idx, appeach.length)) {
+    const { isRyanWin, scoreGap } = compareScore(ryan, appeach);
+    if (!isRyanWin) return;
+
+    if (maxScoreGap > scoreGap) return;
+    if (maxScoreGap < scoreGap) {
+      maxScoreGap = scoreGap;
+      return (answer = [...ryan]);
     }
+    if (compareSameScore(ryan)) {
+      maxScoreGap = scoreGap;
+      answer = [...ryan];
+    }
+
     return;
   }
-  const accArrow = sum(arr);
-  if (curIdx === 10 && accArrow <= ArrowLimit) return DFS(curIdx + 1, [...arr, ArrowLimit - accArrow]);
-  if (accArrow + Apeach[curIdx] + 1 <= ArrowLimit) DFS(curIdx + 1, [...arr, Apeach[curIdx] + 1]);
-  DFS(curIdx + 1, [...arr, 0]);
+  const requiredArrows = appeach[idx] + 1;
+  if (isLastIdx(idx, appeach.length)) return DFS(idx + 1, 0, [...ryan, remainedArrows], appeach);
+  if (requiredArrows <= remainedArrows) DFS(idx + 1, remainedArrows - requiredArrows, [...ryan, requiredArrows], appeach);
+  DFS(idx + 1, remainedArrows, [...ryan, 0], appeach);
 }
+
 function solution(n, info) {
-  Apeach = info;
-  ArrowLimit = n;
-  for (let i = 0; i <= 10; i++) {
-    const arr = Array.from({ length: i }, () => 0);
-    DFS(i, arr);
-  }
+  DFS(0, n, [], info);
   return answer.length === 0 ? [-1] : answer;
 }
