ecf54b1547acede00aedbd4a5caefa140415854c
By_juun <neostgeart@gmail.com>
2023-01-01 20:23:48 +0900

    23/1/1

--- BaekJoon/14500.js
+++ BaekJoon/14500.js
@@ -1,3 +1,6 @@
+const dx = [-1, 0, 0, 1];
+const dy = [0, -1, 1, 0];
+const strToNum = (str) => str.split(" ").map(Number);
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
@@ -7,60 +10,46 @@ let input = [];
 rl.on("line", function (line) {
   input.push(line);
 }).on("close", async function () {
-  // 답안 작성
-  let answer = "";
-  answer = tetro(input);
-  console.log(answer);
-  process.exit();
+  const graph = [];
+  input.slice(1).forEach((row) => {
+    graph.push(strToNum(row));
+  });
 });
 
-let tetro = function (input) {
-  let tetromino = [];
-  let tmp = input.splice(0, 1).join("").split(" ");
-  let N = tmp[0] * 1;
-  let M = tmp[1] * 1;
-  //위,아래 여백용 알파 배열
-  let alpha = new Array(M + 4).fill(0);
-  tetromino.push(alpha);
-  input.reduce((acc, cur) => {
-    //좌, 우에도 필요한 만큼 여백을 넣어준다
-    tetromino.push(("0 " + cur + " 0 0 0").split(" "));
-  }, "");
-  tetromino.push(alpha);
-  tetromino.push(alpha);
-  tetromino.push(alpha);
-  return tetro_max(tetromino, N, M);
-};
-
-let tetro_max = function (t, N, M) {
-  let result = 0;
-  //console.log(t)
-  for (i = 1; i <= N; i++) {
-    for (j = 1; j <= M; j++) {
-      //현재까지의 max값과 그 외 19가지 테트로미노 경우의 수들 중 MAX를 구한다.
-      result = Math.max(
-        result,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 2][j] * 1 + t[i + 3][j] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i][j + 2] * 1 + t[i][j + 3] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i][j + 1] * 1 + t[i + 1][j + 1] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 2][j] * 1 + t[i + 2][j + 1] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i][j + 1] * 1 + t[i][j + 2] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 1][j + 1] * 1 + t[i + 1][j + 2] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 2][j] * 1 + t[i + 2][j - 1] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i + 1][j + 1] * 1 + t[i + 2][j + 1] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i][j + 2] * 1 + t[i - 1][j + 2] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i][j + 2] * 1 + t[i + 1][j + 2] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i + 1][j] * 1 + t[i + 2][j] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 1][j + 1] * 1 + t[i + 2][j + 1] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i - 1][j + 1] * 1 + t[i - 1][j + 2] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 1][j - 1] * 1 + t[i + 2][j - 1] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i + 1][j + 1] * 1 + t[i + 1][j + 2] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i - 1][j + 1] * 1 + t[i][j + 2] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 1][j + 1] * 1 + t[i + 2][j] * 1,
-        t[i][j] * 1 + t[i][j + 1] * 1 + t[i][j + 2] * 1 + t[i + 1][j + 1] * 1,
-        t[i][j] * 1 + t[i + 1][j] * 1 + t[i + 1][j - 1] * 1 + t[i + 2][j] * 1
-      );
+function solve(graph) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < graph.length; i++) {
+    for (let j = 0; j < graph[0].length; j++) {
+      const result = BFS(i, j, graph);
     }
   }
-  return result;
-};
+}
+
+function BFS(i, j, graph) {
+  let result = Number.MIN_SAFE_INTEGER;
+  const row = graph.length;
+  const col = graph[0].length;
+  const isVisited = Array.from(
+    { length: row },
+    () => Array.from({ length: col }),
+    () => false
+  );
+  isVisited[i][j] = true;
+  const queue = [{ x: i, y: j, sum: 0, count: 0 }];
+  while (queue.length !== 0) {
+    const curPos = queue.shift();
+    if (count === 4) {
+      result = Math.max(result, curPos.sum);
+      continue;
+    }
+    for (let dir = 0; dir < 4; dir++) {
+      const nextX = curPos.x + dx[dir];
+      const nextY = curPos.y + dy[dir];
+      if (nextX >= 0 && nextX < row && nextY >= 0 && nextY < col) {
+        if (!isVisited[nextX][nextY]) {
+          queue.push({ x: nextX, y: nextY, sum: curPos.sum + graph[nextX][nextY], count: curPos.count + 1 });
+        }
+      }
+    }
+  }
+}
