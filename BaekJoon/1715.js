d5a402f7590a8f2e1f2036120235bd15e39f0adf
By_juun <neostgeart@gmail.com>
2023-03-23 00:05:55 +0900

    23/03/23

--- BaekJoon/1715.js
+++ BaekJoon/1715.js
@@ -3,70 +3,76 @@ class MinHeap {
   constructor() {
     this.heap = [];
   }
-  push(val) {
-    this.heap.push(val);
-    let curIdx = this.heap.length - 1;
-    let parentIdx = Math.floor((curIdx - 1) / 2);
-    const { heap } = this;
-    while (curIdx > 0 && heap[parentIdx] > heap[curIdx]) {
-      this.swap(parentIdx, curIdx);
-      curIdx = parentIdx;
-      parentIdx = Math.floor((curIdx - 1) / 2);
+
+  push(value) {
+    this.heap.push(value);
+
+    let idx = this.heap.length - 1;
+    let parentIdx = this.getParentIdx(idx);
+
+    while (idx > 0 && this.heap[idx] < this.heap[parentIdx]) {
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+      parentIdx = this.getParentIdx(idx);
     }
   }
-  pop() {
-    const { heap } = this;
-    if (heap.length === 0) return undefined;
-    if (heap.length === 1) return heap.shift();
 
-    const result = heap[0];
-    heap[0] = heap.pop();
+  pop() {
+    if (this.heap.length === 0) throw Error();
+    else if (this.heap.length === 1) return this.heap.shift();
+
+    const result = this.heap[0];
+
+    this.heap[0] = this.heap.pop();
+
+    let originIdx = 0;
     let idx = 0;
+    let leftChildIdx = this.getLeftChildIdx(idx);
+    let rightChildIdx = this.getRightChildIdx(idx);
 
     while (true) {
-      const originIdx = idx;
-      let leftParentIdx = this.getLeftParentIdx(idx);
-      let rightParentIdx = this.getRightParentIdx(idx);
-
-      if (heap[leftParentIdx] < heap[idx]) idx = leftParentIdx;
-      if (heap[rightParentIdx] < heap[idx]) idx = rightParentIdx;
+      if (leftChildIdx < this.heap.length && this.heap[idx] > this.heap[leftChildIdx]) idx = leftChildIdx;
+      if (rightChildIdx < this.heap.length && this.heap[idx] > this.heap[rightChildIdx]) idx = rightChildIdx;
 
       if (idx === originIdx) break;
-      this.swap(originIdx, idx);
+      this.swap(idx, originIdx);
+      originIdx = idx;
+      leftChildIdx = this.getLeftChildIdx(idx);
+      rightChildIdx = this.getRightChildIdx(idx);
     }
+
     return result;
   }
 
+  getParentIdx(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  getLeftChildIdx(idx) {
+    return idx * 2 + 1;
+  }
+
+  getRightChildIdx(idx) {
+    return idx * 2 + 2;
+  }
   swap(idx1, idx2) {
     const temp = this.heap[idx1];
     this.heap[idx1] = this.heap[idx2];
     this.heap[idx2] = temp;
   }
-
-  getLeftParentIdx(idx) {
-    return idx * 2 + 1;
-  }
-  getRightParentIdx(idx) {
-    return idx * 2 + 2;
-  }
 }
-
-function solution(pair) {
+function solve(pair) {
   let result = 0;
   const minHeap = new MinHeap();
-  pair.forEach((node) => {
-    minHeap.push(node);
-  });
+  pair.forEach((num) => minHeap.push(num));
 
-  //console.log(minHeap.heap);
-
-  while (minHeap.heap.length > 1) {
-    const firstPop = minHeap.pop();
-    const SecondPop = minHeap.pop();
-    result += firstPop + SecondPop;
-    minHeap.push(firstPop + SecondPop);
+  while (minHeap.heap.length !== 1) {
+    const newPair = minHeap.pop() + minHeap.pop();
+    result += newPair;
+    minHeap.push(newPair);
   }
-  return result;
+
+  console.log(result);
 }
 
 const readline = require("readline");
@@ -76,6 +82,5 @@ rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
   const pair = input.slice(1).map(Number);
-  const result = solution(pair);
-  console.log(result);
+  solve(pair);
 });
