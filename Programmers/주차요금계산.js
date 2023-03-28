9632b2a2809c9cb9119d57cdf5725aac8141de05
By_juun <neostgeart@gmail.com>
2023-03-28 13:29:45 +0900

    23/03/28-2

--- Programmers/주차요금계산.js
+++ Programmers/주차요금계산.js
@@ -1,34 +1,58 @@
-function getMinutes(time) {
-  const [hour, minutes] = time.split(":").map(Number);
-  return hour * 60 + minutes;
-}
 function solution(fees, records) {
   var answer = [];
-  const endTime = "23:59";
+
   const [defaultMinute, defaultPrice, unitMinute, unitPrice] = fees;
-  const recordInfo = {};
-  for (const record of records) {
-    const [time, carNum, status] = record.split(" ");
-    if (status === "IN") {
-      if (!recordInfo[carNum]) recordInfo[carNum] = { time: getMinutes(time), status, acc: 0 };
-      else recordInfo[carNum] = { ...recordInfo[carNum], time: getMinutes(time), status };
-    } else {
-      const { time: startTime, acc } = recordInfo[carNum];
-      recordInfo[carNum] = { acc: acc + getMinutes(time) - startTime, status, time };
-    }
-  }
-  for (const key of Object.keys(recordInfo)) {
-    const { acc, status, time } = recordInfo[key];
-    if (status === "IN") {
-      recordInfo[key] = { acc: acc + getMinutes(endTime) - time, status: "OUT" };
-    }
-  }
-  for (const key of Object.keys(recordInfo)) {
-    const { acc } = recordInfo[key];
-    if (acc <= defaultMinute) answer.push({ key, price: defaultPrice });
-    else {
-      answer.push({ key, price: defaultPrice + Math.ceil((acc - defaultMinute) / unitMinute) * unitPrice });
-    }
-  }
-  return answer.sort((a, b) => Number(a.key) - Number(b.key)).map((v) => v.price);
+
+  const recordMap = new Map();
+  const accTime = new Map();
+
+  const convertToMinute = (time) => {
+    const [hour, minute] = time.split(":").map(Number);
+    return hour * 60 + minute;
+  };
+
+  const addCarToMap = (time, carNum) => recordMap.set(carNum, time);
+
+  const removeCarFromMap = (time, carNum) => {
+    const timeGap = time - recordMap.get(carNum);
+    if (accTime.has(carNum)) accTime.set(carNum, accTime.get(carNum) + timeGap);
+    else accTime.set(carNum, timeGap);
+    recordMap.delete(carNum);
+  };
+
+  const calculatePrice = (minute) =>
+    minute <= defaultMinute
+      ? defaultPrice
+      : defaultPrice + Math.ceil((minute - defaultMinute) / unitMinute) * unitPrice;
+
+  records.forEach((record) => {
+    const [time, carNum, type] = record.split(" ");
+    const fn = type === "IN" ? addCarToMap : removeCarFromMap;
+    fn(convertToMinute(time), carNum);
+  });
+
+  [...recordMap.keys()].forEach((key) => removeCarFromMap(23 * 60 + 59, key));
+
+  [...accTime.keys()]
+    .sort((a, b) => a - b)
+    .forEach((key) => {
+      answer.push(calculatePrice(accTime.get(key)));
+    });
+
+  return answer;
 }
+
+solution(
+  [180, 5000, 10, 600],
+  [
+    "05:34 5961 IN",
+    "06:00 0000 IN",
+    "06:34 0000 OUT",
+    "07:59 5961 OUT",
+    "07:59 0148 IN",
+    "18:59 0000 IN",
+    "19:09 0148 OUT",
+    "22:59 5961 IN",
+    "23:00 5961 OUT",
+  ]
+);
