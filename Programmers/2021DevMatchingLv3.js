2598012c77f287f3b08d1ee9df18df19b7197747
By_juun <neostgeart@gmail.com>
2023-03-29 18:43:11 +0900

    23/03/29-2

--- Programmers/2021DevMatchingLv3.js
+++ Programmers/2021DevMatchingLv3.js
@@ -1,28 +1,49 @@
-function solution(enroll, referral, seller, amount) {
-  var answer = [];
-  const user = {};
-  user["center"] = { referral: null, amount: 0 };
-  for (let i = 0; i < enroll.length; i++) {
-    user[enroll[i]] = { referral: referral[i] !== "-" ? referral[i] : "center", amount: 0 };
+class User {
+  referral;
+  benifit;
+  constructor(referral) {
+    this.referral = referral;
+    this.benifit = 0;
   }
+}
 
-  for (let i = 0; i < seller.length; i++) {
-    let curUser = user[seller[i]];
-    let curAmount = amount[i] * 100;
-    while (curUser.referral !== null) {
-      if (Math.floor(curAmount / 10) === 0) {
-        curUser.amount += curAmount;
-        break;
-      } else {
-        curUser.amount += curAmount - Math.floor(curAmount / 10);
-        curAmount = Math.floor(curAmount / 10);
-        curUser = user[curUser.referral];
+const isInteger = (num) => Math.floor(num) === num;
+const isReferralExist = (user) => (user.referral ? true : false);
+
+function solution(enroll, referral, seller, amount) {
+  const userMap = new Map();
+  const root = makeUser("root", null);
+
+  makeLink();
+  doCalculate();
+  return [...userMap.values()].map((v) => v.benifit).slice(1);
+
+  function doCalculate() {
+    for (let idx = 0; idx < seller.length; idx++) {
+      let price = amount[idx] * 100;
+      let curUser = userMap.get(seller[idx]);
+
+      while (true) {
+        let remainPrice = Math.floor(price / 10);
+        if (remainPrice < 1 || !isReferralExist(curUser)) {
+          curUser.benifit += price;
+          break;
+        }
+        curUser.benifit += price - remainPrice;
+        price = remainPrice;
+        curUser = curUser.referral;
       }
     }
   }
-  for (const User in user) {
-    answer.push(user[User].amount);
+
+  function makeLink() {
+    for (let idx = 0; idx < enroll.length; idx++)
+      makeUser(enroll[idx], referral[idx] === "-" ? root : userMap.get(referral[idx]));
   }
-  answer.shift();
-  return answer;
+
+  function makeUser(enroll, referral) {
+    const user = new User(referral);
+    userMap.set(enroll, user);
+    return user;
+  }
 }
