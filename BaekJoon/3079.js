c3c583c7c0994084e6ea75779078467a4b2f0df3
By_juun <78716842+BY-juun@users.noreply.github.com>
2023-04-10 15:57:00 +0900

    23/04/10-4

--- BaekJoon/3079.js
+++ BaekJoon/3079.js
@@ -1,26 +1,36 @@
-function solution(m, arr) {
-	if (arr.length === 1) {
-		console.log(arr[0] * m);
-		return;
-	}
-	let start = 0;
-	let end = m * Math.max(...arr)
-	while (start <= end) {
-		let mid = Math.floor((start + end) / 2);
-		let total = arr.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
-		if (total >= m) end = mid - 1;
-		else start = mid + 1;
-	}
-	console.log(end + 1);
-}
-
 const readline = require("readline");
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 let input = [];
 rl.on("line", function (line) {
-	input.push(line);
+  input.push(line);
 }).on("close", function () {
-	const [N, M] = input[0].split(" ").map(Number);
-	const arr = input.slice(1).map(Number);
-	solution(M, arr);
+  const [N, M] = input[0].split(" ").map(Number);
+  const arr = input.slice(1).map(Number);
+  solution(M, arr);
 });
+
+function solution(m, arr) {
+  if (arr.length === 1) {
+    console.log(arr[0] * m);
+    return;
+  }
+  let left = 0;
+  let right = Math.max(...arr) * m;
+
+  while (left <= right) {
+    let mid = Math.floor((right + left) / 2);
+
+    const canHandleCount = arr.reduce(
+      (acc, cur) => acc + Math.floor(mid / cur),
+      0
+    );
+
+    if (canHandleCount < m) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  console.log(right + 1);
+}
