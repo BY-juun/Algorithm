c33abc84f86d65aeefa02fd667191f728b7f8b29
By_juun <78716842+BY-juun@users.noreply.github.com>
2023-04-12 13:27:54 +0900

    23/04/12-3

--- BaekJoon/2470.js
+++ BaekJoon/2470.js
@@ -1,31 +1,37 @@
-function solve(items) {
-  let minDiff = Number.MAX_SAFE_INTEGER;
-  items.sort((a, b) => a - b);
-  let answer = [items[0], items[items.length - 1]];
-  let left = 0;
-  let right = items.length - 1;
-  while (left < right) {
-    const leftItem = items[left];
-    const rightItem = items[right];
-    const diff = leftItem + rightItem;
-    if (minDiff > Math.abs(diff)) {
-      minDiff = Math.abs(diff);
-      answer = [leftItem, rightItem];
-    }
-    if (diff > 0) right--;
-    else if (diff < 0) left++;
-    else break;
-  }
-  console.log(answer.join(" "));
-}
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const strToNum = (str) => str.split(' ').map(Number);
 
-const strToBigInt = (str) => str.split(" ").map(Number);
-const readline = require("readline");
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-let input = [];
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  const items = strToBigInt(input[1]);
-  solve(items);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+let input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(strToNum(input[1]).sort((a, b) => a - b));
+});
+function solution(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  let answer_sum = Number.MAX_SAFE_INTEGER;
+  let answer = [0, 0];
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === 0) return console.log(`${arr[left]} ${arr[right]}`);
+
+    if (Math.abs(sum) < answer_sum) {
+      answer_sum = Math.abs(sum);
+      answer = [arr[left], arr[right]];
+    }
+
+    if (sum > 0) right--;
+    else left++;
+  }
+
+  console.log(`${answer[0]} ${answer[1]}`);
+}
